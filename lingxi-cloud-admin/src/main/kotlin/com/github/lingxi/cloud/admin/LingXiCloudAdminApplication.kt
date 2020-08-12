738a7fcbee2879789e211ea71b60db6039ea91dd
anonymou_s@163.com <anonymou_s@163.com>
2020-08-12 20:15:38 +0800

    lingxi-cloud-admin

---
+++ lingxi-cloud-admin/src/main/kotlin/com/github/lingxi/cloud/admin/LingXiCloudAdminApplication.kt
@@ -0,0 +1,11 @@
+package com.github.lingxi.cloud.admin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LingXiCloudAdminApplication
+
+fun main(args: Array<String>) {
+    runApplication<LingXiCloudAdminApplication>(*args)
+}
